48cf6e0d4263773c4d33ca1c8884dfcd80de18fc
Micah Nyabuto <atembamicah@gmail.com>
2025-04-20 21:28:40 +0300

    Update

--- app/src/main/java/com/micahnyabuto/spendsmart/MainActivity.kt
+++ app/src/main/java/com/micahnyabuto/spendsmart/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.micahnyabuto.spendsmart.ui.theme.SpendSmartTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,6 +20,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SpendSmartTheme {
+                Text("Lets do this",
+                    modifier = Modifier.padding(16.dp)
+                        .fillMaxSize(),
+
+
+                )
 
             }
         }
