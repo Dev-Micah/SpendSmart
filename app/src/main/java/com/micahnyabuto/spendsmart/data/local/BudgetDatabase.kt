621d31a7ed00a76bcc6a952827d1841c66e47cc0
Micah Nyabuto <atembamicah@gmail.com>
2025-04-20 21:30:52 +0300

    Updated Database

--- app/src/main/java/com/micahnyabuto/spendsmart/data/local/BudgetDatabase.kt
+++ app/src/main/java/com/micahnyabuto/spendsmart/data/local/BudgetDatabase.kt
@@ -2,7 +2,7 @@ package com.micahnyabuto.spendsmart.data.local
 
 import androidx.room.Database
 
-@Database(entities =[BudgetEntity::class], version =1)
+@Database(entities =[TransactionEntity::class,BudgetEntity::class], version =1)
 abstract class BudgetDatabase {
     abstract fun budgetDao(): BudgetDao
     abstract fun transactionDao(): TransactionDao
