e67f7c63d2e23e4f0d9ce63016278c5660fa24a6
Micah Nyabuto <atembamicah@gmail.com>
2025-04-21 23:53:26 +0300

    Added navigation destinations

---
+++ app/src/main/java/com/micahnyabuto/spendsmart/ui/navigation/Destinations.kt
@@ -0,0 +1,25 @@
+package com.micahnyabuto.spendsmart.ui.navigation
+
+import kotlinx.serialization.Serializable
+
+sealed class Destinations {
+    @Serializable
+    object Home
+
+    @Serializable
+    object Settings
+
+    @Serializable
+    object AddBudgetItem
+
+    @Serializable
+    object Profile
+
+
+    @Serializable
+    data class BudgetAdded(
+        val budgetItemName: String,
+        val amount: String,
+        val date : String
+    )
+}
