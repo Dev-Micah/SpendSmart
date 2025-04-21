1775d0f19a993bdb2179b61073654e378f3c1a4e
Micah Nyabuto <atembamicah@gmail.com>
2025-04-21 23:55:39 +0300

    Added bottom navigation

--- app/src/main/java/com/micahnyabuto/spendsmart/ui/navigation/BottomNavigation.kt
+++ app/src/main/java/com/micahnyabuto/spendsmart/ui/navigation/BottomNavigation.kt
@@ -1,2 +1,24 @@
 package com.micahnyabuto.spendsmart.ui.navigation
 
+import com.micahnyabuto.spendsmart.R
+
+
+enum class BottomNavigation(
+    val label: String,
+    val selectedIcon: Int,
+    val unselectedIcon: Int,
+    val route : Any
+){
+    Home(
+        label = "Home",
+        selectedIcon = R.drawable.ic_home,
+        unselectedIcon = R.drawable.ic_home,
+        route = Destinations.Home
+    ),
+    Settings(
+        label = "Settings",
+        selectedIcon = R.drawable.ic_settings,
+        unselectedIcon = R.drawable.ic_settings,
+        route = Destinations.Settings
+    )
+}
