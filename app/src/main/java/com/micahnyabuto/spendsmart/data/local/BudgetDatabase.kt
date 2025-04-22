25a4f15a219a055916d394df0dd444bd9a71b564
Micah Nyabuto <atembamicah@gmail.com>
2025-04-22 19:38:26 +0300

    cleaned up the Database class

--- app/src/main/java/com/micahnyabuto/spendsmart/data/local/BudgetDatabase.kt
+++ app/src/main/java/com/micahnyabuto/spendsmart/data/local/BudgetDatabase.kt
@@ -1,9 +1,10 @@
 package com.micahnyabuto.spendsmart.data.local
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(entities =[TransactionEntity::class,BudgetEntity::class], version =1)
-abstract class BudgetDatabase {
+abstract class BudgetDatabase : RoomDatabase(){
     abstract fun budgetDao(): BudgetDao
     abstract fun transactionDao(): TransactionDao
 }
