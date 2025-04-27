b4c49262e524600c1ca06e46e95c3ce22c1da1a8
Micah Nyabuto <atembamicah@gmail.com>
2025-04-27 20:59:31 +0300

    Added Settings screen

--- app/src/main/java/com/micahnyabuto/spendsmart/ui/Screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/micahnyabuto/spendsmart/ui/Screens/settings/SettingsScreen.kt
@@ -1,22 +1,20 @@
 package com.micahnyabuto.spendsmart.ui.Screens.settings
 
-import android.annotation.SuppressLint
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
-import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -26,93 +24,93 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.micahnyabuto.spendsmart.ui.navigation.Destinations
 
-
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingsScreen(
-    modifier: Modifier = Modifier,
-    navController: NavController,
+    modifier: Modifier= Modifier,
+    navController: NavController
 ){
     var isDarkMode by remember { mutableStateOf(false) }
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                navigationIcon = {
-                    Icon(
-                        imageVector = Icons.Default.ArrowBackIosNew,
-                        contentDescription = "Navigate back",
-                        modifier= Modifier
-                            .clickable{navController.navigate(Destinations.Home)}
+    var isDarkModeToggle by remember { mutableStateOf(false) }
 
-                    )
-                },
-                title = {
-                    Text("Settings",
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 24.sp)
-                }
-            )
-        }
-    ){
-        Surface (
-            modifier= Modifier.fillMaxSize(),
-            color =if(isDarkMode) Color.Black else Color.White
-        ){
-            Column {
-                DarkModeRow(
-                    isDarkMode = isDarkMode,
-                    onDarkModeToggle = { isDarkMode = it },
-                )
-            }
-        }
-
-        }
-
-    }
-@Composable
-fun DarkModeRow(
-    isDarkMode: Boolean,
-    onDarkModeToggle: (Boolean) -> Unit
-) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Text(
-            text = "Dark Mode",
-            style = MaterialTheme.typography.bodyLarge,
-            color = MaterialTheme.colorScheme.onBackground
-        )
-
-        Switch(
-            checked = isDarkMode,
-            onCheckedChange = onDarkModeToggle,
-            colors = SwitchDefaults.colors(
-                checkedThumbColor = MaterialTheme.colorScheme.primary,
-                uncheckedThumbColor = MaterialTheme.colorScheme.onSurface
-            )
-        )
-    }
-}
-
-@Preview
-@Composable
-fun SettingScreenPreview(){
-    SettingsScreen(
-        modifier = Modifier,
-        navController = rememberNavController(),
+    SettingsScreenContent(
+        modifier =modifier,
+        isDarkMode = isDarkMode,
+        isDarkModeToggle = isDarkModeToggle,
+        navController = navController
     )
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SettingsScreenContent(
+    modifier: Modifier = Modifier,
+    isDarkMode: Boolean,
+    isDarkModeToggle: Boolean,
+    navController: NavController,
+){
+
+    Scaffold(
+        topBar = {
+           TopAppBar(
+               title = {
+                Text("Settings",
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+                }
+
+           )
+
+
+        }
+    ){paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .padding(8.dp)
+                .fillMaxSize()
+        ) {
+            Card(
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp)
+            ) {
+                Column(
+                    modifier = Modifier.padding(8.dp),
+                    verticalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+                    Text(
+                        text = "Create or sign in to your account for improved security of your budgets",
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                    Button(
+                        shape = RoundedCornerShape(8.dp),
+                        onClick = {navController.navigate(Destinations.Profile)}
+                    ) {
+                        Text(text = "Get started")
+                    }
+
+                }
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = "Enable Dark Theme")
+                Switch(
+                    checked = isDarkMode,
+                    onCheckedChange = {isDarkModeToggle}
+                )
+            }
+            }
+
+        }
+}
+
