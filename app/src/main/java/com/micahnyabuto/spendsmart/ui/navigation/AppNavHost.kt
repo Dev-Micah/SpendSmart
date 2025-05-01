90d3e70cf1bd5e3de4cdacc9fd808ef28194b82a
Micah Nyabuto <atembamicah@gmail.com>
2025-05-01 23:41:25 +0300

    update

--- app/src/main/java/com/micahnyabuto/spendsmart/ui/navigation/AppNavHost.kt
+++ app/src/main/java/com/micahnyabuto/spendsmart/ui/navigation/AppNavHost.kt
@@ -2,10 +2,13 @@ package com.micahnyabuto.spendsmart.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
 import com.micahnyabuto.spendsmart.ui.Screens.addbudget.AddBudgetItemScreen
+import com.micahnyabuto.spendsmart.ui.Screens.addbudget.BudgetAddedScreen
 import com.micahnyabuto.spendsmart.ui.Screens.home.HomeScreen
 import com.micahnyabuto.spendsmart.ui.Screens.profile.ProfileScreen
 import com.micahnyabuto.spendsmart.ui.Screens.reports.ReportsScreen
@@ -46,6 +49,18 @@ fun AppNavHost(
         composable <Destinations.AddBudgetItem>{
             AddBudgetItemScreen(
                 navController =navController
+            )
+        }
+        composable<Destinations.BudgetAdded> { backStackEntry ->
+            val budgetItemName = backStackEntry.toRoute<Destinations.BudgetAdded>().budgetItemName
+            val amount =backStackEntry.toRoute<Destinations.BudgetAdded>().amount
+            val date =backStackEntry.toRoute<Destinations.BudgetAdded>().date
+            BudgetAddedScreen(
+                budgetItemName =budgetItemName,
+                amount = amount,
+                date = date,
+                navController = navController,
+                modifier = modifier
             )
         }
     }
